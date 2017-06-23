5a43439a5c1e64809d2caae267eb0c70e27c28f7
magthyhui <244626523@qq.com>
2017-06-23 16:23:20 +0800

    改写404 500错误 优化连接池

---
+++ src/main/java/com/gdky/restful/config/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.gdky.restful.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gdky.restful.entity.ResponseMessage;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 系统异常处理，比如：404,500
+     * @param req
+     * @param resp
+     * @param e
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ResponseMessage defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        logger.error("", e);
+        String message = e.getMessage();
+        if (e instanceof org.springframework.web.servlet.NoHandlerFoundException) {
+        	String url = ((org.springframework.web.servlet.NoHandlerFoundException) e).getRequestURL();
+             return ResponseMessage.error("404", "访问路径:"+url+" 不存在！");
+        } else {
+            return ResponseMessage.error("500", message);
+        }
+    }
+}
